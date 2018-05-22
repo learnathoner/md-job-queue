d967e8ceeae17618345994bdadf7487600854b32
learnathoner <mike.tikh@gmail.com>
2018-05-22 15:55:47 -0500

    Initialize post/jobs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sequelize: db } = require('./database/index');
+const { sequelize: db } = require('../database/index');
 
 const app = express();
 
@@ -8,6 +8,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
+
+/**
+ * Post new jobs
+ * @param {Object} req.body - contains JSON { url: jobURL }
+ *
+ * @return {number} jobID
+ */
+app.post('/jobs', (req, res) => {
+  const { url } = req.body;
+  // TODO: Check if url valid input. Normalize url structure?
+  // Try adding URL to db, if already in there return jobID
+  const jobID = ''; /* jobID from db */
+  res.send(jobID);
+});
+
+app.get('/', (req, res) => {
+  res.send('default');
+});
 
 const port = process.env.PORT || 3000;
 
